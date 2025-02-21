256660cb4791bd4beb2886aff55867e7204af95b
xolosArmy Network <contacto@xolosramirez.com>
2025-02-21 10:27:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -30,28 +30,10 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 
-    // Populate business list
-    const businessList = document.getElementById('business-list');
-    const businesses = [
-        {
-            name: 'Xolos Ramirez',
-            description: 'El mejor criadero de perro prehispánico xoloitzcuintle en México.',
-            url: 'https://www.xolosramirez.com'
-        }
-    ];
-
-    businesses.forEach(business => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `
-            <h3>${business.name}</h3>
-            <p>${business.description}</p>
-            <a href="${business.url}" target="_blank">${business.url}</a>
-        `;
-        businessList.appendChild(listItem);
-    });
 
     memoFeed();
 });
+
 
  const memoFeed = async () => {
     const url = 'https://graph.cash/graphql';
