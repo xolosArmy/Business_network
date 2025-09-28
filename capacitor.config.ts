59fe3d59d77eb8a8e66107b1d8b528649b754171
RMZ TECHNOLOGIES <contacto@xolosramirez.com>
2025-09-28 11:20:45 -0600

    Update Capacitor configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,10 @@
-const config = {
-  appId: "com.rmz.wallet",
-  appName: "RMZ Wallet",
-  webDir: "www",
-  bundledWebRuntime: false,
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rmz.wallet',
+  appName: 'RMZ Wallet',
+  webDir: 'www',
+  bundledWebRuntime: false
 };
 
 export default config;
