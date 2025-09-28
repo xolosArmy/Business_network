177b4fde6a8a6bc0981d02601d3e8032dd8254b2
RMZ TECHNOLOGIES <contacto@xolosramirez.com>
2025-09-28 11:17:28 -0600

    refactor: replace react native stack with angular dependencies

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,33 +1,6 @@
-import { encode, decode } from 'base-64';
-
-if (!global.btoa) {
-  global.btoa = encode;
-}
-
-if (!global.atob) {
-  global.atob = decode;
-}
-
-import 'react-native-url-polyfill/auto';
-import 'text-encoding-polyfill';
-import 'react-native-get-random-values';
-
-import { install as installQuickCrypto } from 'react-native-quick-crypto';
-
-installQuickCrypto();
-
-import { Buffer } from 'buffer';
-
-if (!global.Buffer) {
-  global.Buffer = Buffer;
-}
-
-if (typeof global.TextEncoder === 'undefined' &&
-    typeof globalThis.TextEncoder !== 'undefined') {
-  global.TextEncoder = globalThis.TextEncoder;
-}
-
-if (typeof global.TextDecoder === 'undefined' &&
-    typeof globalThis.TextDecoder !== 'undefined') {
-  global.TextDecoder = globalThis.TextDecoder;
-}
+/**
+ * Polyfills for the Angular web build.
+ * Modern browsers already provide the necessary Web APIs,
+ * so this file intentionally exports nothing.
+ */
+export {};
