b425f520a3ee845e7f27e25b67d65c4f80cbe5e5
RMZ TECHNOLOGIES <contacto@xolosramirez.com>
2025-10-04 12:11:04 -0600

    Configure BluetoothLe plugin strings

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,16 @@ const config: CapacitorConfig = {
   appId: 'com.rmz.wallet',
   appName: 'RMZ Wallet',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    BluetoothLe: {
+      displayStrings: {
+        scanning: 'Escaneando dispositivos BLE...',
+        scanAgain: 'Escanear otra vez',
+        cancel: 'Cancelar',
+      },
+    },
+  },
 };
 
 export default config;
