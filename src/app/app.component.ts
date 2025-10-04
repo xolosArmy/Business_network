a461a74f7fc0100cf613d4733bc4c4b177fefef5
RMZ TECHNOLOGIES <contacto@xolosramirez.com>
2025-10-04 11:32:26 -0600

    Enable sync service on app init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PwaService } from './services/pwa.service';
 import { EnviarService } from './services/enviar.service';
+import { SyncService } from './services/sync.service';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,14 @@ import { EnviarService } from './services/enviar.service';
   `,
 })
 export class AppComponent implements OnInit {
-  constructor(private pwa: PwaService, private enviarService: EnviarService) {}
+  constructor(
+    private pwa: PwaService,
+    private enviarService: EnviarService,
+    private sync: SyncService,
+  ) {}
 
   ngOnInit() {
+    this.sync.listenForNetwork();
     setTimeout(() => this.pwa.showInstallPrompt(), 5000);
     void this.enviarService.processPendingTransactions();
   }
