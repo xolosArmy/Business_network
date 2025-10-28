17e76b46cbce31b1903d4baacbd0435d3c494a4e
RMZ TECHNOLOGIES <contacto@xolosramirez.com>
2025-10-28 09:28:01 -0600

    Add browser polyfills for Buffer and process

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,10 @@
+import { Buffer } from 'buffer';
+import process from 'process';
+
+(window as any).global = window;
+(window as any).Buffer = Buffer;
+(window as any).process = process;
+
 /**
  * Polyfills for the Angular web build.
  * Modern browsers already provide the necessary Web APIs,
